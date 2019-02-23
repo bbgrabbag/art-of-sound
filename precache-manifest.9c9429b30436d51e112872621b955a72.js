9f18dd0a2c456aba9772b2e1a2d25e643f35430f
Ben Turner <bbgrabbag@gmail.com>
2019-02-22 17:48:30 -0700

    ready for github-pages

---
+++ precache-manifest.9c9429b30436d51e112872621b955a72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8e9e9fcfbc2f75a3195d",
+    "url": "/static/js/main.8e9e9fcf.chunk.js"
+  },
+  {
+    "revision": "103e05a0efec56812072",
+    "url": "/static/js/2.103e05a0.chunk.js"
+  },
+  {
+    "revision": "85d9bbb1375ac37ac60a3633a82009ba",
+    "url": "/index.html"
+  }
+];
